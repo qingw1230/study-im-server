0ba1640db71ec9cafb0377e26811be683f3c7617
qingw1230 <1366502108@qq.com>
2025-01-31 16:30:46 +0800

    feat: 添加 reply proto 文件

--- pkg/proto/gen.sh
+++ pkg/proto/gen.sh
@@ -4,3 +4,4 @@ protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=p
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative group/group.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative msg/msg.proto
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative push/push.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative reply/reply.proto
